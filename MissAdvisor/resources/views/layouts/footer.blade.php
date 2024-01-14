a94b2ed67d5adc5de172eb3a6f145674ae0851e4
Munashe Chapata <munachaps@gmail.com>
2024-01-14 15:50:30 +0200

    Updated landing page

---
+++ MissAdvisor/resources/views/layouts/footer.blade.php
@@ -0,0 +1,124 @@
+<footer>
+    <div class="container">
+        <div class="f-items default-padding">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 item">
+                    <div class="f-item about">
+                        <h4 class="widget-title">About</h4>
+                        <p>
+                            We are an international education cusultancy that offers assistance 
+                            in overseas education application and securing scholarships. 
+                        </p>
+                        <div class="social">
+                            <h5>Follow Us</h5>
+                            <ul>
+                                <li class="facebook">
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                </li>
+                                <li class="twitter">
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                </li>
+                                <li class="instagram">
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                </li>
+                                <li class="linkedin">
+                                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 item">
+                    <div class="f-item link">
+                        <h4 class="widget-title">Usefull Links</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Study Abroad</a>
+                            </li>
+                            <li>
+                                <a href="#">Program and Fees</a>
+                            </li>
+                          
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 item">
+                    <div class="f-item link">
+                        <h4 class="widget-title">Support</h4>
+                        <ul>
+                            <li>
+                                <a href="#">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Our Services</a>
+                            </li>
+                           
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 item">
+                    <div class="f-item contact">
+                        <h4 class="widget-title">Contact Info</h4>
+                        <div class="address">
+                            <ul>
+                                <li>
+                                    <strong>Email:</strong> info@missadvisor.com
+                                </li>
+                                <li>
+                                    <strong>Contact:</strong> +263-772-732-449
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="opening-info">
+                            <h5>Opening Hours</h5>
+                            <ul>
+                                <li> <span> Mon - Tues :  </span>
+                                  <div class="float-right"> 8.00 am - 5.00 pm </div>
+                                </li>
+                                <li> <span> Wednes - Frid :</span>
+                                  <div class="float-right"> 8.00 am - 5.00 pm </div>
+                                </li>
+                                <li> <span> Sun : </span>
+                                  <div class="float-right closed"> Closed </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Bottom -->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>&copy; Copyright 
+                        @php
+                         echo date('Y')
+                       @endphp All Rights Reserved by <a href="#"></a></p>
+                </div>
+                <div class="col-lg-6 text-right link">
+                    <ul>
+                        <li>
+                            <a href="#">Terms</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy</a>
+                        </li>
+                        <li>
+                            <a href="#">Support</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Footer Bottom -->
+</footer>
---
+++ MissAdvisor/resources/views/layouts/header.blade.php
@@ -0,0 +1,69 @@
+  <!-- Start Navigation -->
+  <nav class="navbar navbar-default attr-border navbar-fixed white no-background bootsnav">
+
+    <!-- Start Top Search -->
+    <div class="container">
+        <div class="row">
+            <div class="top-search">
+                <div class="input-group">
+                    <form action="#">
+                        <input type="text" name="text" class="form-control" placeholder="Search">
+                        <button type="submit">
+                            <i class="ti-search"></i>
+                        </button>  
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Top Search -->
+
+    <div class="container">
+
+        <!-- Start Atribute Navigation -->
+        <div class="attr-nav">
+            <ul>
+                <li class="search"><a href="#"><i class="ti-search"></i></a></li>
+            </ul>
+        </div>        
+        <!-- End Atribute Navigation -->
+
+        <!-- Start Header Navigation -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="" style="color: #fff;
+                                                   font-weight:1000;
+                                                   margin-top:15%">
+                Miss Advisor<img src="" class="logo " >
+            </a>
+        </div>
+        <!-- End Header Navigation -->
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar-menu">
+            <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle active" data-toggle="dropdown" >Study Abroad</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" >Work Abroad</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" >Programs and Fees</a>
+                </li>
+                
+                <li>
+                    <a href="contact.html">About Us</a>
+                </li>
+               
+                <li>
+                    <a href="contact.html">Contact Us</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div>
+
+</nav>
+<!-- End Navigation -->
